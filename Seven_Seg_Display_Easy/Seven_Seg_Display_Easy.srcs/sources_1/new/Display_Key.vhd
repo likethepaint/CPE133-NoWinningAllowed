ae4f3d8bf4696f059934bc62bc9480bf3fe5870b
Collin Kenner <Collin Kenner>
2016-12-04 22:50:16 -0700

    completed seven seg display output general

--- Seven_Seg_Display_Easy/Seven_Seg_Display_Easy.srcs/sources_1/new/Display_Key.vhd
+++ Seven_Seg_Display_Easy/Seven_Seg_Display_Easy.srcs/sources_1/new/Display_Key.vhd
@@ -1,35 +1,17 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 12/04/2016 06:56:09 PM
--- Design Name: 
--- Module Name: Display_Key - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+--Seven Seg Display output key
+-- by Collin kenner and Brett Glidden
+--This device takes in binary inputs from several different modules in the device
+--depending on the input value the seven seg then displays a word
+--rather than actually send over the 48 bit word bus we opted to just have the key values map to 
+--the desired output words.
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Display_Key is
   Port (   clk : in STD_LOGIC;
@@ -41,19 +23,54 @@ end Display_Key;
 
 architecture Behavioral of Display_Key is
 
--- The current bin_in_* being sampled
-signal key_value : STD_LOGIC_VECTOR(2 downto 0);
+--this is the total word including a space for the scrolling of the word in binary
 signal current_input : STD_LOGIC_VECTOR(47 downto 0);
+--value sent to seven segment display
+signal digit_input : STD_LOGIC_VECTOR(5 downto 0);
 
 -- Signal to allow for SSEG_AN to be sampled
 signal sseg_an_wire : STD_LOGIC_VECTOR(3 downto 0) := "1111";
 begin
+    --based upon a 3 bit input key bus, an output bit representing a word is selected as the seven seg input
     KEY_SELECT: Process(bin_in)
     begin
-        if(bin_in ="000") then
+        if(bin_in = "000") then
             current_input <= "100001001110010101001100011001010111001110111111";--welcome
+        elsif(bin_in = "001") then
+            current_input <= "001011001110010000010010011000111111111111111111";--begin
+        elsif(bin_in = "010") then
+            current_input <= "001110001010011101100011111111111111111111111111";--easy
+        elsif(bin_in = "011") then
+            current_input <= "010111001110001101010010011111010111111111111111";--medium
+        elsif(bin_in = "100") then
+            current_input <= "010001001010011100001101111111111111111111111111";--hard
+        elsif(bin_in = "101") then
+            current_input <= "100001010010011000011000001110011100111111111111";--winner
+        elsif(bin_in = "110") then
+            current_input <= "010101011001011101001110011100111111111111111111";--loser
+        elsif(bin_in = "111") then
+            current_input <= "011100001110011101011110001010011100011110111111";--restart
+        else                  
+            current_input <= "111111111111111111111111111111111111111111111111";--null value
         end if;
-    end process;
+    end process KEY_SELECT;
+    
+    SSEG_SCROLL_METHOD : Process(clk)
+        --possible error here double check if problem occurs-----------------------------------------------------
+        variable placeholder : STD_LOGIC_VECTOR(47 downto 0):= "0";
+        variable clock_divider: integer range 0 to 10000000 := 0;
+    begin
+        if rising_edge(clk) then
+            if(clock_divider = 10000000) then
+                placeholder(5 downto 0) := current_input(47 downto 42);--rotate leftmost bits over
+                placeholder(47 downto 6) := current_input(41 downto 0);--bit shift remaining bits
+                current_input <=  placeholder;
+                clock_divider := 0;
+            else
+                clock_divider := clock_divider +1;
+            end if;    
+        end if;                  
+    end process SSEG_SCROLL_METHOD;
     
     SSEG_80Hz_Refresh: Process(clk)
         variable count: integer range 0 to 250000 := 0;
@@ -64,16 +81,16 @@ begin
                     if (count = 250000) then
                         if ((sseg_an_wire) = "0111" OR (sseg_an_wire = "1111")) then
                             sseg_an_wire <= "1110";
-                           
+                            digit_input <= current_input(29 downto 24);
                         elsif (sseg_an_wire = "1110") then
                             sseg_an_wire <= "1101";
-                    
+                            digit_input <= current_input(35 downto 30);
                         elsif (sseg_an_wire = "1101") then
                             sseg_an_wire <= "1011";
-                          
+                            digit_input <= current_input(41 downto 36);  
                         elsif (sseg_an_wire = "1011") then
                             sseg_an_wire <= "0111";
-                      
+                            digit_input <= current_input(47 downto 42);
                         end if;
                         
                         count := 0;
@@ -82,7 +99,7 @@ begin
                     end if;
                 end if;
             end if;
-        end process;
+        end process SSEG_80Hz_Refresh;
         
         sseg_an <= sseg_an_wire;
             
@@ -91,7 +108,7 @@ begin
             ssegDisplayDriver: process (sseg_an_wire) is
             begin
                 if (cen = '1') then
-                    case current_input is
+                    case digit_input is
                         when "000000" => sseg_cat <= "00000011"; --0
                         when "000001" => sseg_cat <= "10011111"; --1
                         when "000010" => sseg_cat <= "00100101"; --2
